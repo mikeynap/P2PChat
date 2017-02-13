9a46e480965320c8b44c735081cab29d909d5949
Michael Napolitano <mnapolit@cisco.com>
2017-02-13 11:26:08 -0800

    Initial Commit

---
+++ P2PChat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  P2PChat
+//
+//  Created by mnapolit on 2/13/17.
+//  Copyright © 2017 micmoo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
